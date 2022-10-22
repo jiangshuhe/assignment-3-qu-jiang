30802a951663c69b3c559fd27e08f624d0091159
Haoran <hzhang74@usc.edu>
2022-10-22 16:00:41 -0700

    3

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ export default {
   build: {
     target: ["chrome100", "safari15", "firefox100"],
   },
-  // Change this to your repo name
+  // Change this to your repo names
   base: "/assignment-3-qu-jiang/",
 };
